6c752556d280d6a12c530ca4b4a0a6c680ce58b2
Joon Park <joonqkr@berkeley.edu>
2021-06-07 10:11:28 -0400

    update .gitignore and got working driver for Selenium

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import requests
+# import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-most_recent_tourn = "2021 LCK Spring"
+most_recent_tourn = "LCK 2020 Summer"
 
 def get_page():
     """Asks the user for a player name and returns page data of match
@@ -10,8 +11,8 @@ def get_page():
 
     Returns
     -------
-    Response object
-        The data on the page
+    Driver
+        The driver that has url of page
     """
 
     name = input("Player name: ")
@@ -26,24 +27,31 @@ def get_page():
     # url with player name and tournament
     url = f'https://lol.fandom.com/wiki/Special:RunQuery/MatchHistoryPlayer?pfRunQueryFormName=MatchHistoryPlayer&MHP%5Bpreload%5D=Player&MHP%5Btournament%5D={most_recent_tourn}&MHP%5Blink%5D={name}&MHP%5Bchampion%5D=&MHP%5Brole%5D=&MHP%5Bteam%5D=&MHP%5Bpatch%5D=&MHP%5Byear%5D=&MHP%5Bregion%5D=&MHP%5Btournamentlevel%5D=&MHP%5Brecord%5D=&MHP%5Brecordorder%5D%5Bis_checkbox%5D=true&MHP%5Bitem%5D=&MHP%5Bjungleitem%5D=&MHP%5Bjungleenchant%5D=&MHP%5Brune%5D=&MHP%5Blimit%5D=50&MHP%5Bwhere%5D=&MHP%5Bincludelink%5D%5Bis_checkbox%5D=true&MHP%5Btextonly%5D%5Bis_checkbox%5D=true&wpRunQuery=Run+query&pf_free_text='
 
-    # get page / Response object of specified url
-    page = requests.get(url)
+    # set driver to get url
+    driver = webdriver.Chrome('env/chromedriver')
+    driver.get(url)
 
-    return page
+    return driver
 
-def solo_data():
+def solo_data(driver):
     """Returns data on one player when only one player is input
 
     Parameters
     ----------
-    page : Response object
-        The match history page of one player
+    driver : Driver
+        The driver that has url of page
 
     Returns
     -------
     dictionary
         The player's stats (KDA, champion usage, itemizations, etc.)
     """
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    table = soup.find_all('table', class_='mhplayer')
+    print(table)
+
+    driver.quit()
     return
 
 def duo_data():
@@ -67,4 +75,4 @@ def duo_data():
 if __name__ == "__main__":
     player_page = get_page()
     data = solo_data(player_page)
-    print(data)
+    # print(data)
