c3dd5cad0f44151427e8be193f58cbff1439e25c
Joon Park <joonqkr@berkeley.edu>
2021-06-08 23:38:12 -0400

    can get data with only pandas, removed selenium and beautifulsoup

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-# import BeautifulSoup, Selenium, and pandas
-from bs4 import BeautifulSoup
-from selenium import webdriver
+# import pandas
 import pandas as pd
 
-most_recent_tourn = "LCK 2020 Summer"
+most_recent_tourn = "LCK%202020%20Summer"
 
-def get_page():
-    """Asks the user for a player name and returns page data of match
+def get_df():
+    """Asks the user for a player name and returns data frame of match
     history of that player
     (player name, tournament name)
 
     Returns
     -------
-    Driver
-        The driver that has url of page
+    Pandas data frame
+        The data frame of the match history of input player and tournament
     """
 
     name = input("Player name: ")
+
+    # tourn = input("Tournament name: ")
 
     # Check if input name is a valid name
     # if :
@@ -28,18 +28,18 @@ def get_page():
     # url with player name and tournament
     url = f'https://lol.fandom.com/wiki/Special:RunQuery/MatchHistoryPlayer?pfRunQueryFormName=MatchHistoryPlayer&MHP%5Bpreload%5D=Player&MHP%5Btournament%5D={most_recent_tourn}&MHP%5Blink%5D={name}&MHP%5Bchampion%5D=&MHP%5Brole%5D=&MHP%5Bteam%5D=&MHP%5Bpatch%5D=&MHP%5Byear%5D=&MHP%5Bregion%5D=&MHP%5Btournamentlevel%5D=&MHP%5Brecord%5D=&MHP%5Brecordorder%5D%5Bis_checkbox%5D=true&MHP%5Bitem%5D=&MHP%5Bjungleitem%5D=&MHP%5Bjungleenchant%5D=&MHP%5Brune%5D=&MHP%5Blimit%5D=50&MHP%5Bwhere%5D=&MHP%5Bincludelink%5D%5Bis_checkbox%5D=true&MHP%5Btextonly%5D%5Bis_checkbox%5D=true&wpRunQuery=Run+query&pf_free_text='
 
-    # set driver to get url
-    driver = webdriver.Chrome('env/chromedriver')
-    driver.get(url)
+    dfs = pd.read_html(url)
 
-    return driver
+    print(dfs[0])
 
-def solo_data(driver):
+    return dfs[0]
+
+def solo_data(df):
     """Returns data on one player when only one player is input
 
     Parameters
     ----------
-    driver : Driver
+    df : Pandas data frame
         The driver that has url of page
 
     Returns
@@ -48,13 +48,8 @@ def solo_data(driver):
         The player's stats (KDA, champion usage, itemizations, etc.)
     """
 
-    dfs = pd.read_html(driver.page_source)
-    # soup = BeautifulSoup(driver.page_source, 'html.parser')
-    # table = soup.find_all('table', class_='mhplayer')
-    # print(table)
-    print(dfs)
 
-    driver.quit()
+
     return
 
 def duo_data():
@@ -76,6 +71,8 @@ def duo_data():
     return
 
 if __name__ == "__main__":
-    player_page = get_page()
-    data = solo_data(player_page)
+    # player_page = get_page()
+    # data = solo_data(player_page)
+
+    get_df()
     # print(data)
